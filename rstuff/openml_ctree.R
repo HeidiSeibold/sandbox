70803230ef95f3a43fcdab7a97b9405e6005a4b9
HeidiSeibold <heidi.seibold@uzh.ch>
2016-12-07 12:18:46 +0100

    ctree openml stuff

--- rstuff/openml_ctree.R
+++ rstuff/openml_ctree.R
@@ -3,17 +3,52 @@ library("OpenML")
 library("partyNG")
 source("new_ctree_mlr.R")
 
+## parallelization
+# library("parallelMap")
+# ncores <- parallel::detectCores() - 1
+# parallelStartSocket(ncores)
+# parallelSource("new_ctree_mlr.R")
 
-## get task infos
-taskinfo <- listOMLTasks(task.type = "Supervised Classification")[1:2, ]
-tasks <- lapply(taskinfo$task.id, getOMLTask)
-names(tasks) <- taskinfo$task.id
+library("parallel")
+ncores <- detectCores() - 1
+
+
+## get task infos and keep only relevant ones
+taskinfo_all <- listOMLTasks(task.type = "Supervised Classification")
+
+# less than 30 percent missing
+fewmissings <- taskinfo_all$number.of.instances.with.missing.values / 
+  taskinfo_all$number.of.instances < 0.3
+
+# nobs in (80, 1Mio)
+goodnobs <- taskinfo_all$number.of.instances > 80 &
+  taskinfo_all$number.of.instances < 10^6
+
+# nfeatures in (3, 50)
+goodnfeat <- taskinfo_all$number.of.features > 3 &
+  taskinfo_all$number.of.features < 50
+
+# no leave-one-out CV
+noloo <- taskinfo_all$estimation.procedure != "Leave one out"
+
+taskinfo_relevant <- taskinfo_all[fewmissings & goodnobs & goodnfeat & noloo, ]
+
+# each data set only once
+dup <- duplicated(taskinfo_relevant$data.id)
+taskinfo <- taskinfo_relevant[!dup, ]
+
+## obtain list of relevant tasks
+set.seed(123)
+taskinfo <- taskinfo[sample(1:nrow(taskinfo), size = 2), ]
+tid <- taskinfo$task.id
+tasks <- lapply(tid, getOMLTask)
+names(tasks) <- tid
 
 ## create leraners
 lrn.list <- list(
   makeLearner("classif.newctree", teststat = "quadratic", splitstat = "quadratic"),
-  makeLearner("classif.newctree", teststat = "quadratic", splitstat = "maximum"),
-  makeLearner("classif.newctree", teststat = "maximum", splitstat = "quadratic"),
+  # makeLearner("classif.newctree", teststat = "quadratic", splitstat = "maximum"),
+  # makeLearner("classif.newctree", teststat = "maximum", splitstat = "quadratic"),
   makeLearner("classif.newctree", teststat = "maximum", splitstat = "maximum")
 )
 
@@ -23,19 +58,14 @@ grid <- expand.grid(task.id = taskinfo$task.id,
 
 
 ## run learners on tasks
-# runs <- lapply(seq_row(grid), function(i) {
-#   message(i)
-#   runTaskMlr(task = tasks[[grid$task.id[i]]], 
-#              learner = lrn.list[[grid$lrn.ind[i]]])
-# })
+run_lt <- function(i) {
+  message(i)
+  runTaskMlr(task = tasks[[as.character(grid$task.id[i])]],
+             learner = lrn.list[[grid$lrn.ind[i]]])
+}
+
+# runs <- lapply(seq_row(grid), run_lt)
+runs <- mclapply(seq_row(grid), run_lt,
+                 mc.cores = ncores)
 
 
-problem <- runTaskMlr(task = tasks[[grid$task.id[8]]], 
-                      learner = lrn.list[[grid$lrn.ind[8]]])
-
-dat <- tasks[[grid$task.id[8]]]$input$data.set$data
-save(dat, file = "problemdata.RData")
-
-summary(dat)
-tr1 <- partyNG::ctree(class ~ ., dat, control = ctree_control(teststat = "quadratic", splitstat = "maximum"))
-tr4 <- partyNG::ctree(class ~ ., dat, control = ctree_control(teststat = "maximum", splitstat = "quadratic"))
--- rstuff/problemdata.R
+++ rstuff/problemdata.R
@@ -7,13 +7,28 @@ n_nas <- colSums(dat_nas)
 (frac_nas <- n_nas/NROW(dat))
 datn <- dat[ , frac_nas < 0.2]
 
+## drop unused levels
+datn <- droplevels(datn)
+
 summary(datn)
+datn$product.type <- NULL
 
-(tr1 <- partyNG::ctree(class ~ ., datn, control = ctree_control(teststat = "quadratic", splitstat = "maximum", maxdepth = 2)))
-(tr2 <- partyNG::ctree(class ~ ., datn, control = ctree_control(teststat = "maximum", splitstat = "quadratic", maxdepth = 2)))
+## compute trees
+(tr1 <- partyNG::ctree(class ~ ., datn, 
+                       control = ctree_control(teststat = "quadratic", 
+                                               splitstat = "maximum", 
+                                               maxdepth = 2)))
+(tr2 <- partyNG::ctree(class ~ ., datn, 
+                       control = ctree_control(teststat = "maximum", 
+                                               splitstat = "quadratic", 
+                                               maxdepth = 2)))
 
-(ctr1 <- partykit::ctree(class ~ ., datn, control = partykit::ctree_control(teststat = "quad", maxdepth = 2)))
-(ctr2 <- partykit::ctree(class ~ ., datn, control = partykit::ctree_control(teststat = "max", maxdepth = 2)))
+(ctr1 <- partykit::ctree(class ~ ., datn, 
+                         control = partykit::ctree_control(teststat = "quad", 
+                                                           maxdepth = 2)))
+(ctr2 <- partykit::ctree(class ~ ., datn, 
+                         control = partykit::ctree_control(teststat = "max", 
+                                                           maxdepth = 2)))
 
 # Problem with tr2 in doTest() mit
 # ret <- .Call("R_MaxtypeTest", object, as.integer(alt), 
