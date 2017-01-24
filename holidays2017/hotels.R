f11aca125a5bcdb2d69e7825a1147a8a1de64168
HeidiSeibold <heidi.seibold@uzh.ch>
2017-01-24 10:17:28 +0100

    get lat and long from link to google maps in booking

--- holidays2017/hotels.R
+++ holidays2017/hotels.R
@@ -21,7 +21,15 @@ url_wishlist <- sapply(X = b_urlsp, FUN = grep, pattern = "http", value = TRUE)
 sites <- lapply(url_wishlist, readLines)
 
 site <- sites[[1]]
-grep("streetAddress", site, value = TRUE)
+grep('"hasMap"', site, value = TRUE)
+
+coord_raw <- sapply(sites, grep, pattern = '"hasMap"', value = TRUE)
+cr <- strsplit(coord_raw, split = '&')
+coord_mark <- sapply(cr, grep, pattern = "markers=", value = TRUE)
+coord_char0 <- sub("^(.+?)7c", "", coord_mark)
+coord_char <- strsplit(coord_char0, split = ",")
+coord <- t(sapply(coord_char, as.numeric))
+colnames(coord) <- c("lat_booking", "long_booking")
 
 addresses_raw <- sapply(sites, grep, pattern = "streetAddress", value = TRUE)
 ar <- strsplit(addresses_raw, split = '"')
@@ -35,5 +43,6 @@ gc_towns <- geocode(towns)
 
 gc_addresses[is_na_add, ] <- gc_towns[is_na_add, ]
 
-hotels <- data.frame(url_wishlist, addresses, gc_addresses, good_address = !is_na_add)
+hotels <- data.frame(url_wishlist, addresses, gc_addresses, 
+                     good_address = !is_na_add, coord)
 write.csv(hotels, "hotels.csv", row.names = FALSE)
