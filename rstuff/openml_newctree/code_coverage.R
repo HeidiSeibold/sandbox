72bffec863be7b6783599de7e756efd51c818fb5
HeidiSeibold <heidi.seibold@uzh.ch>
2017-02-14 14:56:13 +0100

    nice code coverage

--- rstuff/openml_newctree/code_coverage.R
+++ rstuff/openml_newctree/code_coverage.R
@@ -3,12 +3,25 @@ getwd()
 pkg_dir <- "~/partykit/pkg/devel/partykit"
 tst_dir <- "~/sandbox/rstuff/openml_newctree/"
 knitr::purl("OpenML_test_ctree.Rmd")
+excle <- list("R/as.party.R",
+              "R/glmtree.R",
+              "R/lmtree.R",
+              "R/meanvartree.R",
+              "R/mob-plot.R",
+              "R/modelparty2.R",
+              "R/modelparty.R",
+              "R/mtree.R",
+              "R/palmtree.R",
+              "R/plot.R",
+              "R/pmmlTreeModel.R",
+              "R/print.R",
+              "R/simpleparty.R",
+              "R/utils.R",
+              "R/varimp.R")
 pc <- package_coverage(path = pkg_dir, type = "none", 
-                       code = "OpenML_test_ctree.R")
-pc <- package_coverage(path = pkg_dir, type = "none", 
-                       code = file.path(tst_dir, "OpenML_test_ctree.R"))
-pc <- package_coverage(path = pkg_dir, type = "none", 
-                       code = paste0(tst_dir, "OpenML_test_ctree.R"))
-pc <- package_coverage(path = pkg_dir, type = "none", 
-                       code = "source('OpenML_test_ctree.R', echo = TRUE)")
+                       code = "source('OpenML_test_ctree.R', echo = TRUE)",
+                       exclusions = excl)
 pc
+save(pc, file = "code_coverage.rda")
+
+zero_coverage(pc)
