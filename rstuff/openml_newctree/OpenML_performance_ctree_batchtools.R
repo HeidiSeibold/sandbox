105077220d69cdd6c499f59229840a7fe02c7738
HeidiSeibold <heidi.seibold@uzh.ch>
2017-03-09 09:00:38 +0100

    getting ready to run a bigger study

--- rstuff/openml_newctree/OpenML_performance_ctree_batchtools.R
+++ rstuff/openml_newctree/OpenML_performance_ctree_batchtools.R
@@ -7,14 +7,28 @@ load(file = "results_regr_batchtools.rda")
 eval_regr <- listOMLRunEvaluations(run.id = res_regr$run.id)
 runeval_regr <- cbind(res_regr, eval_regr)
 
-ggplot(runeval_regr, aes(x = interaction(splittest, lookahead, intersplit, nmax), 
-                         color = data.name, group = interaction(data.name, learner), linetype = learner)) +
-  geom_point(aes(y = mean.absolute.error)) + geom_line(aes(y = mean.absolute.error))
+p_regr <- ggplot(runeval_regr, 
+                 aes(x = interaction(splittest, lookahead, intersplit, nmax), 
+                     color = data.name, group = interaction(data.name, learner), 
+                     linetype = learner)) +
+  geom_point(aes(y = mean.absolute.error)) + 
+  geom_line(aes(y = mean.absolute.error))
 
 
+load(file = "results_classif_batchtools.rda")
 
 eval_classif <- listOMLRunEvaluations(run.id = res_classif$run.id)
 runeval_classif <- cbind(res_classif, eval_classif)
 
-ggplot(runeval_classif, aes(x = parameter, color = data.name, group = interaction(data.name, type), linetype = type)) +
-  geom_point(aes(y = predictive.accuracy)) + geom_line(aes(y = predictive.accuracy))
+p_classif <- ggplot(runeval_classif, 
+                    aes(x = interaction(splittest, lookahead, intersplit, nmax), 
+                        color = data.name, group = interaction(data.name, learner), 
+                        linetype = learner)) +
+  geom_point(aes(y = predictive.accuracy)) + 
+  geom_line(aes(y = predictive.accuracy))
+
+
+ggsave(p_regr, filename = "OpenML_performance_ctree_regr.pdf", 
+       width = 11, height = 6)
+ggsave(p_classif, filename = "OpenML_performance_ctree_classif.pdf", 
+       width = 11, height = 6)
--- rstuff/openml_newctree/code_coverage.R
+++ rstuff/openml_newctree/code_coverage.R
@@ -1,27 +1,49 @@
 library("covr")
-getwd()
 pkg_dir <- "~/partykit/pkg/devel/partykit"
-tst_dir <- "~/sandbox/rstuff/openml_newctree/"
-knitr::purl("OpenML_test_ctree.Rmd")
 excl <- list("R/as.party.R",
-              "R/glmtree.R",
-              "R/lmtree.R",
-              "R/meanvartree.R",
-              "R/mob-plot.R",
-              "R/modelparty2.R",
-              "R/modelparty.R",
-              "R/mtree.R",
-              "R/palmtree.R",
-              "R/plot.R",
-              "R/pmmlTreeModel.R",
-              "R/print.R",
-              "R/simpleparty.R",
-              "R/utils.R",
-              "R/varimp.R")
+             "R/glmtree.R",
+             "R/lmtree.R",
+             "R/meanvartree.R",
+             "R/mob-plot.R",
+             "R/modelparty2.R",
+             "R/modelparty.R",
+             "R/mtree.R",
+             "R/palmtree.R",
+             "R/plot.R",
+             "R/pmmlTreeModel.R",
+             "R/print.R",
+             "R/simpleparty.R",
+             "R/utils.R",
+             "R/varimp.R")
 pc <- package_coverage(path = pkg_dir, type = "none", 
-                       code = "source('OpenML_test_ctree.R', echo = TRUE)",
+                       code = "source('OpenML_test_ctree_batchtools.R', echo = TRUE)",
                        line_exclusions = excl)
-pc
 save(pc, file = "code_coverage.rda")
 
-zero_coverage(pc)
+# library("covr")
+# getwd()
+# pkg_dir <- "~/partykit/pkg/devel/partykit"
+# tst_dir <- "~/sandbox/rstuff/openml_newctree/"
+# knitr::purl("OpenML_test_ctree.Rmd")
+# excl <- list("R/as.party.R",
+#               "R/glmtree.R",
+#               "R/lmtree.R",
+#               "R/meanvartree.R",
+#               "R/mob-plot.R",
+#               "R/modelparty2.R",
+#               "R/modelparty.R",
+#               "R/mtree.R",
+#               "R/palmtree.R",
+#               "R/plot.R",
+#               "R/pmmlTreeModel.R",
+#               "R/print.R",
+#               "R/simpleparty.R",
+#               "R/utils.R",
+#               "R/varimp.R")
+# pc <- package_coverage(path = pkg_dir, type = "none", 
+#                        code = "source('OpenML_test_ctree.R', echo = TRUE)",
+#                        line_exclusions = excl)
+# pc
+# save(pc, file = "code_coverage.rda")
+# 
+# zero_coverage(pc)
